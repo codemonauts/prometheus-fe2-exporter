96010eae2725e37fbd1dc9ea8b48b6079ca39fd1
Thomas Schulze <tom@codemonauts.com>
2026-08-18 13:31:50 +0200

    add slog, make listen port configurable, add http server for for metrics, healthz and index

--- main.go
+++ main.go
@@ -1,25 +1,31 @@
 package main
 
 import (
+	"context"
 	"errors"
-	"flag"
-	"fmt"
+	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"flag"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 const (
-	listenAddr = ":9865"
-	namespace  = "fe2"
+	defaultListenAddr = ":9865"
+	namespace         = "fe2"
 )
 
 var (
-	url       string
-	accessKey string
-	up        = prometheus.NewDesc(
+	url        string
+	accessKey  string
+	listenAddr string
+	up         = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "", "up"),
 		"Was the last scrape successful",
 		nil, nil,
@@ -79,32 +85,69 @@ var (
 func init() {
 	envUrl := os.Getenv("FE2_EXPORTER_URL")
 	envAccesskey := os.Getenv("FE2_EXPORTER_ACCESSKEY")
+	envListen := os.Getenv("FE2_EXPORTER_LISTEN")
+	if envListen == "" {
+		envListen = defaultListenAddr
+	}
 
 	flag.StringVar(&url, "url", envUrl, "Address of the FE2 server")
 	flag.StringVar(&accessKey, "accesskey", envAccesskey, "Authorization key for the monitoring api")
+	flag.StringVar(&listenAddr, "listen", envListen, "Address to listen on for the metrics endpoint")
 }
 
 func main() {
-
 	flag.Parse()
 
 	if url == "" || accessKey == "" {
-		fmt.Println("Url and accesskey are both required parameters")
+		slog.Error("url and accesskey are both required parameters")
 		os.Exit(1)
 	}
 
-	fmt.Printf("FE2 server address is %q\n", url)
+	slog.Info("starting exporter", "server", url, "listen", listenAddr)
 
 	exporter := NewExporter(url, accessKey)
 	prometheus.MustRegister(exporter)
 
-	fmt.Printf("Listening on %q\n", listenAddr)
-	http.Handle("/metrics", promhttp.Handler())
-	err := http.ListenAndServe(listenAddr, nil)
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("Exporter closed\n")
-	} else if err != nil {
-		fmt.Printf("Error starting exporter: %s\n", err)
-		os.Exit(1)
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		_, _ = w.Write([]byte(`<html>
+<head><title>FE2 Exporter</title></head>
+<body><h1>FE2 Exporter</h1><p><a href="/metrics">Metrics</a></p></body>
+</html>`))
+	})
+
+	server := &http.Server{
+		Addr:              listenAddr,
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	// Shut down cleanly on SIGINT/SIGTERM.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("error starting exporter", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	<-ctx.Done()
+	slog.Info("shutting down")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		slog.Error("error during shutdown", "error", err)
 	}
 }
